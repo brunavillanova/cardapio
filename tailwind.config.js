e1333bbc2f273eaf8212fe2f37ae3f669f076962
bruna villanova <brunnavillanova@gmail.com>
2024-09-04 17:00:50 -0300

    Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}', // Inclui todos os arquivos HTML e JS no projeto
+    '!./node_modules/**/*', // Exclui o diretório node_modules
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        "home": "url('/assets/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
